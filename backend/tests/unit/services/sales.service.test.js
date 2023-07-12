420e2c484d532b65eba397a724186622789f836b
LucasArent <arentlucas2708@gmail.com>
2023-07-12 17:07:10 -0300

    testes da sales service pra 50%

--- backend/tests/unit/services/sales.service.test.js
+++ backend/tests/unit/services/sales.service.test.js
@@ -5,7 +5,7 @@ const {
     saleAllProducstDB,
     saleAllProductsModel,
     saleProductIdFromDB,
-    // saleProductIdFromModel,
+    saleProductsIdFromModel,
 } = require('../mocks/sale.product.mock');
 const salesService = require('../../../src/services/sales.service');
 
@@ -27,6 +27,42 @@ describe('teste sale dos service', function () {
         expect(serviceResponse.status).to.be.equal('SUCCESSFUL');
         expect(serviceResponse.data).to.be.deep.equal(saleProductIdFromDB);
         });
+
+    it('testa se registra uma nova venda', async function () {
+        sinon.stub(salesModel, 'createSale').resolves(saleProductsIdFromModel);
+
+        const inputData = [
+            { productId: 1, quantity: 1 },
+            { productId: 2, quantity: 5 },
+        ];
+        const serviceResponse = await salesService.createProductSale(inputData);
+
+        expect(serviceResponse.status).to.be.equal('CREATED');
+        expect(serviceResponse.data).to.be.deep.equal(saleProductsIdFromModel);
+    });
+
+    it('testa se retorna erro ao registrar uma nova venda', async function () {
+        const inputData = [
+            { productId: 1, quantity: 1 },
+            { productId: 2, quantity: 0 },
+        ];
+        const serviceResponse = await salesService.createProductSale(inputData);
+
+        expect(serviceResponse.status).to.be.equal('INVALID_VALUE');
+        expect(serviceResponse.data).to.be.deep.equal({ 
+            message: '"quantity" must be greater than or equal to 1', 
+        });
+    });
+    it('testa se retorna NOT_FOUND quando não acha o id', async function () {
+        const inputData = [
+            { productId: 39, quantity: 1 },
+            { productId: 2, quantity: 5 },
+        ];
+        const serviceResponse = await salesService.createProductSale(inputData);
+
+        expect(serviceResponse.status).to.be.equal('NOT_FOUND');
+        expect(serviceResponse.data).to.be.deep.equal({ message: 'Product not found' });
+    });
     afterEach(function () {
         sinon.restore();
     });
