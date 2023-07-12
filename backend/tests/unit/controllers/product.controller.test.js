640e437ec1f6b26e6ffba08e1b8b6fcfeff1cb9f
LucasArent <arentlucas2708@gmail.com>
2023-07-12 15:07:02 -0300

    arrumando erros

--- backend/tests/unit/controllers/product.controller.test.js
+++ backend/tests/unit/controllers/product.controller.test.js
@@ -1,35 +1,35 @@
-// const chai = require('chai');
-// const sinon = require('sinon');
-// const sinonChai = require('sinon-chai');
+const chai = require('chai');
+const sinon = require('sinon');
+const sinonChai = require('sinon-chai');
 
-// const { expect } = chai;
-// chai.use(sinonChai);
+const { expect } = chai;
+chai.use(sinonChai);
 
-// const { productService } = require('../../../src/services');
-// const {   
-//     allProductsModel,
-//     allproductsFromServiceSuccess,
-// } = require('../mocks/product.model.mock');
+const { productService } = require('../../../src/services');
+const {   
+    allProductsModel,
+    allproductsFromServiceSuccess,
+} = require('../mocks/product.model.mock');
 
-// const { productController } = require('../../../src/controllers');
+const { productController } = require('../../../src/controllers');
 
-// describe('testes do Controller', function () {
-//     it('consegue recuperar todos os produtos', async function () {
-//         sinon.stub(productService, 'findAll').resolves(allproductsFromServiceSuccess);
+describe('testes do Controller', function () {
+    it('consegue recuperar todos os produtos', async function () {
+        sinon.stub(productService, 'findAll').resolves(allproductsFromServiceSuccess);
 
-//         const req = {};
-//         const res = {
-//             status: sinon.stub().returnsThis(),
-//             json: sinon.stub(),
-//         };
+        const req = {};
+        const res = {
+            status: sinon.stub().returnsThis(),
+            json: sinon.stub(),
+        };
+        console.log(res);
+        await productController.allProducts(req, res);
+        expect(res.status).to.have.been.calledWith(200);
+        expect(res.json).to.have.been.calledWith(allProductsModel);
+    });
 
-//         await productController.findAll(req, res);
-//         expect(res.status).to.have.been.calledWith(200);
-//         expect(res.json).to.have.been.calledWith(allProductsModel);
-//     });
-
-//     afterEach(function () {
-//         sinon.restore();
-//     });
-// });
+    afterEach(function () {
+        sinon.restore();
+    });
+});
 
